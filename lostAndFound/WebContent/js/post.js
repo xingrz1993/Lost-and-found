3bdf01e717d0e0f21056371c171fcfe27d89fe61
YantianYou <yantian@kth.se>
2017-03-05 18:11:30 +0100

    Get and Post, check the lost and found property, it always sends lost property to the server

--- lostAndFound/WebContent/js/post.js
+++ lostAndFound/WebContent/js/post.js
@@ -2,18 +2,19 @@
 
 	$("#post_btom").click(function(){
 		var item = {
-			"property":($("input[name='property']")).val(),
-			"title":($("input[name='title']")).val(),
+			property:($("input[name='property']")).val(),
+			title:($("input[name='title']")).val(),
 	//		"category":($("select[name='category']")).val(),
-			"date":($("input[name='date']")).val(),
-			"description":$("textarea[name='description']").val()
+			date:($("input[name='date']")).val(),
+			description:$("textarea[name='description']").val()
 		};
 		console.log(item);
 		var itemStr = JSON.stringify(item);
 		$.ajax({
-		type:'POST',
-		url:'excuteAjaxJsonAction',
+		type:"post",
+		url:"excutePostJsonAction",
 		data: itemStr,
+		dataType:"json",
 		success: function(data){console.log(data)},
 		error: function(jqXHR){console.log(jqXHR)},
 		});
--- lostAndFound/src/lostAndFound/com/action/JsonAction.java
+++ lostAndFound/src/lostAndFound/com/action/JsonAction.java
@@ -39,6 +39,39 @@ public class JsonAction extends ActionSupport implements ServletRequestAware{
 	 * ¥¶¿Ìajax«Î«Û
 	 * @return SUCCESS
 	 */
+	/*public String excuteAjax(){
+		String ret = ERROR;
+		 Connection conn = null;
+		 //Statement stmt = null;
+		 String URL = "jdbc:mysql://localhost/webapp";
+		try{
+			//open database
+	        Class.forName("com.mysql.jdbc.Driver");
+	        conn = DriverManager.getConnection(URL, "root", "yantian94");
+	        //stmt = conn.createStatement();
+	        String sql ="INSERT INTO item (property, title, description, date,userID) VALUES (?, ?, ?, ?,1001)";
+	        PreparedStatement ps = conn.prepareStatement(sql);
+	        ps.setString(1, request.getParameter("property"));
+	        ps.setString(2, request.getParameter("title"));
+	        ps.setString(4, request.getParameter("date"));
+	        ps.setString(3, request.getParameter("description"));
+	        ps.executeUpdate();
+	        ret=SUCCESS;
+		}catch (Exception e) {
+			e.printStackTrace();
+			ret=ERROR;
+		}finally {
+	         if (conn != null) {
+	             try {
+	                conn.close();
+	             } catch (Exception e) {
+	             }
+	          }
+	       }
+		return ret;
+	}*/
+	
+	
 	public String excuteAjax(){
 		String ret = ERROR;
 	    Connection conn = null;
