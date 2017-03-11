7c1619556937d78e5c0cac754c8012ef1d4cfb5f
xingrz1993 <xingrz1993@hotmail.com>
2017-03-11 22:26:43 +0100

    adjustments

--- lostAndFound/WebContent/js/search_result.js
+++ lostAndFound/WebContent/js/search_result.js
@@ -8,7 +8,7 @@ $(function(){
 			var cardsObj = eval('(' + cardsStr + ')');
 			console.log('success',cardsObj);
 				$.each(cardsObj,function(i, item){
-					$cards.append('<div class="card">	<div class="cardposter"> <img class="cardposter" src="'+item.userAvatar+'" alt="Avatar"> <p>'+item.userName+'<p> <i class="material-icons">message</i> </div>	<div class="container">	<h3><b>'+item.title+'</b></h3> <button class="item_'+item.property+'_btom">'+item.property+'</button><p>posted on '+item.postDate+'</p><br><p>'+item.description+'</p></div></div>')
+					$cards.append('<div class="card">	<div class="cardposter"> <img class="cardposter" src="'+item.userAvatar+'" alt="Avatar"> <p>'+item.userName+'<p> <i class="material-icons">message</i> </div>	<div class="card_container">	<h3 class="card_container_title"><b>'+item.title+'</b></h3> <button class="item_'+item.stat+'_bton">'+item.stat+'</button><button class="item_'+item.property+'_bton">'+item.property+'</button><p>posted on '+item.date+'</p><br><p class="card_container_des">'+item.description+'</p></div></div>')
 				})
 			}
 		
